57b39a7c35775de6ba0c6bcbfdff9759804ed959
Loren <lm.velasquez12@gmail.com>
2015-07-06 23:12:07 -0500

    made it colorful, still need to figure out legend

--- Nepal_map_paper.R
+++ Nepal_map_paper.R
@@ -1,64 +1,89 @@
-install.packages('maptools')
+setwd('/home/rebeldroid12/Nepal_TeamViz/merged_data')
+
+install.packages("maptools")
+install.packages("sp")
+install.packages("RColorBrewer")
+install.packages("rgdal")
+install.packages("classInt")
+
 library(maptools)
-
-install.packages('rgdal') #install gdal
-library(rgdal)
-
-install.packages('RColorBrewer')
+library(sp)
 library(RColorBrewer)
+library(rgdal)
+library(classInt)
 
-#install.packages('')
+#details
+getinfo.shape("full_dist_merged_data.shp")
 
-classIntervals <- function(var, n, style="quantile", rtimes=3, ..., intervalClosure=c("left", "right"), dataPrecision=NULL) {
-        if (is.factor(var)) stop("var is categorical")
-        if (!is.numeric(var)) stop("var is not numeric") }
-
-setwd('/home/rebeldroid12/Nepal_TeamViz')
 
 #QGIS: merged swb calculated vars & shp
 #load merged data shapefiles
-districts = readShapeSpatial('merged_data/districts/full_dist_merged_data.shp')
 
-#calculated vars
-#districts_var = read.csv('score_dist_final_20150616.csv', sep = ',', header=TRUE)
-#vdcs_var = read.csv('score_vdc_final_20150616.csv', sep = ',', header=TRUE)
+#read in files (district)
+district_shp = readShapeLines('full_dist_merged_data.shp', proj4string=CRS("+proj=longlat"))
+district_csv = read.csv('score_dist_final_20150616.csv', sep=",", header=TRUE)
+
 
 #make vars/measures numeric & rename
-districts$total_pop = as.numeric(districts$score_dist)
-districts$obs_death = as.numeric(districts$score_di_1)
-districts$obs_injured = as.numeric(districts$score_di_2)
-districts$obs_mmi_max_dist = as.numeric(districts$score_di_3)
-districts$obs_casualty = as.numeric(districts$score_di_4)
-districts$pred_death = as.numeric(districts$score_di_5)
-districts$pred_rate = as.numeric(districts$score_di_6)
-districts$pred_density = as.numeric(districts$score_di_7)
-districts$index_cnt = as.numeric(districts$score_di_8)
-districts$index_rate = as.numeric(districts$score_di_9)
-districts$index_density = as.numeric(districts$score_di10)
-districts$wpredicted = as.numeric(districts$score_di11)
-districts$index_roof = as.numeric(districts$score_di12)
-districts$index_wall = as.numeric(districts$score_di13)
+district_shp$total_pop = as.numeric(as.character(district_shp$score_dist))
+district_shp$obs_death = as.numeric(as.character(district_shp$score_di_1))
+district_shp$obs_injured = as.numeric(as.character(district_shp$score_di_2))
+district_shp$obs_mmi_max_dist = as.numeric(as.character(district_shp$score_di_3))
+district_shp$obs_casualty = as.numeric(as.character(district_shp$score_di_4))
+district_shp$pred_death = as.numeric(as.character(district_shp$score_di_5))
+district_shp$pred_rate = as.numeric(as.character(district_shp$score_di_6))
+district_shp$pred_density = as.numeric(as.character(district_shp$score_di_7))
+district_shp$index_cnt = as.numeric(as.character(district_shp$score_di_8))
+district_shp$index_rate = as.numeric(as.character(district_shp$score_di_9))
+district_shp$index_density = as.numeric(as.character(district_shp$score_di10))
+district_shp$wpredicted = as.numeric(as.character(district_shp$score_di11))
+district_shp$index_roof = as.numeric(as.character(district_shp$score_di12))
+district_shp$index_wall = as.numeric(as.character(district_shp$score_di13))
 
 #change NAs back to zero
-districts$obs_death[is.na(districts$obs_death)] <- 0
-districts$obs_injured[is.na(districts$obs_injured)] <- 0
-districts$obs_mmi_max_dist[is.na(districts$obs_mmi_max_dist)] <- 0
-districts$obs_casualty[is.na(districts$obs_casualty)] <- 0
-districts$pred_death[is.na(districts$pred_death)] <- 0
-districts$pred_rate[is.na(districts$pred_rate)] <- 0
-districts$pred_density[is.na(districts$pred_density)] <- 0
-districts$index_cnt[is.na(districts$index_cnt)] <- 0
-districts$index_rate[is.na(districts$index_rate)] <- 0
-districts$index_density[is.na(districts$index_density)] <- 0
-districts$wpredicted[is.na(districts$wpredicted)] <- 0
-districts$index_roof[is.na(districts$index_roof)] <- 0
-districts$index_wall[is.na(districts$index_wall)] <- 0
+district_shp$obs_death[is.na(district_shp$obs_death)] <- 0
+district_shp$obs_injured[is.na(district_shp$obs_injured)] <- 0
+district_shp$obs_mmi_max_dist[is.na(district_shp$obs_mmi_max_dist)] <- 0
+district_shp$obs_casualty[is.na(district_shp$obs_casualty)] <- 0
+district_shp$pred_death[is.na(district_shp$pred_death)] <- 0
+district_shp$pred_rate[is.na(district_shp$pred_rate)] <- 0
+district_shp$pred_density[is.na(district_shp$pred_density)] <- 0
+district_shp$index_cnt[is.na(district_shp$index_cnt)] <- 0
+district_shp$index_rate[is.na(district_shp$index_rate)] <- 0
+district_shp$index_density[is.na(district_shp$index_density)] <- 0
+district_shp$wpredicted[is.na(district_shp$wpredicted)] <- 0
+district_shp$index_roof[is.na(district_shp$index_roof)] <- 0
+district_shp$index_wall[is.na(district_shp$index_wall)] <- 0
 
 
-#create color breaks (9)
-colors <- brewer.pal(9, "YlOrRd")
-brks<-classIntervals(districts$obs_deaths, n=9, style="quantile")
-brks<- brks$brks
+
+# equal-frequency class intervals
+plotvar <- district_shp@data$obs_death
+nclr <- 5
+plotclr <- brewer.pal(nclr,"BuPu")
+class <- classIntervals(plotvar, nclr, style="quantile")
+colcode <- findColours(class, plotclr)
+
+#district_shp@bbox
+#       min      max
+#x 80.05845 88.20153
+#y 26.34797 30.44739
+
+plot(district_shp)
+plot(district_shp, col=colcode, add=T)
+title(main="Observed Deaths", 
+      sub="Quantile (Equal-Frequency) Class Intervals")
+legend(-117, 44, legend=names(attr(colcode, "table")), 
+       fill=attr(colcode, "palette"), cex=0.6, bty="n")
+
+
+#colors <- brewer.pal(9, "YlOrRd")
+#set breaks for the 9 colors 
+#brks<-classIntervals(district_shp$obs_death, n=9, style="quantile")
+#brks<- brks$brks
+
+plot(district_shp, col=colors[findInterval(district_shp$obs_death, brks,all.inside=TRUE)], axes=F)
+
 
 
 #print map
@@ -66,9 +91,9 @@ png(filename="./merged_data/districts/obs_death.png")
 
 
 #plot prettiness!
-plot(districts, col=gray(districts$obs_death/max(districts$obs_death)))
+plot(districts, col=gray(district_shp$obs_death/max(district_shp$obs_death)))
 title(paste ("Nepal April 2015 Earthquake Observed deaths"))
-#plot(districts, col=colors[findInterval(districts$obs_death, brks,all.inside=TRUE)], axes=F)
+#plot(districts, col=colors[findInterval(district_shp$obs_death, brks,all.inside=TRUE)], axes=F)
 dev.off();
 
 
